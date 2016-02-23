226d2f3180b63595ba56248c6bfaf7d976833eba
unknown <mirzayev.gleb@EH203-12-L.student.ccm.edu>
2016-02-22 21:11:59 -0500

    update

--- src/Trader/MovingAverage.java
+++ src/Trader/MovingAverage.java
@@ -1,3 +1,5 @@
+package trader;
+
 import java.util.ArrayList;
 
 public class MovingAverage {
@@ -6,6 +8,19 @@ public class MovingAverage {
 	{
 		ArrayList<Double> ma = new ArrayList<Double>();
 		return ma;
+	}
+	
+	public static double calcMA(ArrayList<Double> prices, int start, int period)
+	{
+		int N = 0;
+		double sum = 0;
+		int end = Math.min(start+period, prices.size());
+		for(int i = start; i<end;i++)
+		{
+			sum += prices.get(i);
+			N ++;
+		}
+		return sum / (double)N;
 	}
 	
 	//write calculation to figure out moving average
